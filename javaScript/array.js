0365833c3f4b3878a50cd08e2a51c2ead9bd65a3
wagnersistemalima <wagner.sistemalima@gmail.com>
2020-12-10 12:20:03 -0300

    Array

--- javaScript/array.js
+++ javaScript/array.js
@@ -1,5 +1,15 @@
+// Array = vetor = Matriz
+
+// arrays permite armazenar varios tipos de dados em uma unica variavel, separados por indice
+
+
+
 // criando um array, e mostrando um dado pelo indice
 
-const pessoa = ["Wagner", 39, 1.70, true, ["Caes", "Carros"]];
+const pessoa = ["Wagner", 39, 1.70, true, ["Caes", "Carros"]]; // todo array começa na posição [0]
+
+// acessando um dado especifico dentro do array, utilizando o indice
 console.log(pessoa[4]);
+
+// verificando o comprimento do array, utilizando a função .length
 console.log(pessoa.length);
